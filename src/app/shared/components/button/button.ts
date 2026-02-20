bd27a94b52dd084d246d1c977dc4a32b912ccd65
David <gidlos55@gmail.com>
2026-02-20 17:15:17 +0300

    ContentChild

--- src/app/shared/components/button/button.ts
+++ src/app/shared/components/button/button.ts
@@ -1,17 +1,30 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ContentChild,
+  ElementRef,
+  EventEmitter,
+  Input,
+  Output,
+} from '@angular/core';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.html',
   styleUrls: ['./button.scss'],
 })
-export class ButtonComponent {
-  @Input() title = '';
+export class ButtonComponent implements AfterViewInit {
   @Input() isDisabled = false;
 
   @Output() btnClick: EventEmitter<void> = new EventEmitter<void>();
 
+  @ContentChild('buttonContent') btnRef: ElementRef | undefined;
+
   onBtnClick(): void {
     this.btnClick.emit();
+  }
+
+  ngAfterViewInit(): void {
+    console.log(this.btnRef);
   }
 }
